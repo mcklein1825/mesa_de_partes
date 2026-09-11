b40500e240c98baa81b843968a3919b142086945
mcklein1825 <mcklein1825@gmail.com>
2026-09-11 10:44:48 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    // App contiene toda la navegación y gestión documental.
     <App />
   </StrictMode>,
 )
