2ef12c40c25b2ff3b702eae02c39012782b36465
mcklein1825 <mcklein1825@gmail.com>
2026-09-16 08:51:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -102,7 +102,7 @@ type Expediente = {
   archivoTamano?: number
   archivoDescripcion?: string
   documentos?: string
-  modalidadRecepcion?: 'Presencial' | 'Virtual'
+  modalidadRecepcion?: 'Presencial' | 'Virtual' | 'Físico' | 'Plataforma SINAD'
   entregadoA?: string
   documentoSeguimiento?: string
   canalRecepcion?: string
