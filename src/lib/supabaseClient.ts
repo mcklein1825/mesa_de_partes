5e90dee6be34418771a562a55badbcd451a15919
mcklein1825 <mcklein1825@gmail.com>
2026-09-16 10:10:52 -0500

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,13 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://tvrogbemtzdhcqjvfvdj.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2cm9nYmVtdHpkaGNxanZmdmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5NTYzNTIsImV4cCI6MjEwNDUzMjM1Mn0.z0vwH9xwyBHWo8ea5GbNx1XHAHXF3ApwBpqfbqiF9aM';
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Faltan las variables de entorno de Supabase')
-}
-
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
